83d3cd481e1786119eabd5fbc712b663dd73c717
Cypherpunk-7 <sbouchaour@guardiaschool.fr>
2024-11-28 12:57:01 +0100

    commit 5

---
+++ Projets.php
@@ -0,0 +1,20 @@
+<?php
+require_once
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <titile> Projets </title>
+    <link rel="stylesheet">
+  </head>
+
+<body>
+  <div>
+    <h1> PRESENTATION DES PROJETS<h1>
+    <?php foreach($dataprojets as $projets): ?>
+      <ul>
+        <li><?php echo $projets
+  <div>

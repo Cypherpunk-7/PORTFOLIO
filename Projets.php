7a4dfbc936219334f7a4facedb19c2831bd877a8
gabrielsavouret <113886771+gabrielsavouret@users.noreply.github.com>
2024-11-28 16:38:04 +0100

    Add files via upload

--- Projets.php
+++ Projets.php
@@ -13,25 +13,28 @@ require_once(__DIR__ . '/variable.php');
 </head>
 <body>
 <header>
-      <!-- Icône hamburger -->
-      <div class="menu-icon" id="menu-icon">&#9776;</div>
-    </header>
+    <!-- Icône hamburger -->
+    <div class="menu-icon" id="menu-icon">&#9776;</div>
+</header>
 
     <!-- Menu latéral -->
-    <nav class="side-nav" id="side-nav">
-      <ul class="links">
+<nav class="side-nav" id="side-nav">
+    <ul class="links">
         <li><a href="/index.html"><strong>HOME</strong></a></li>
         <li><a href="Porjets.php"><strong>PROJETS</strong></a></li>
-      </ul>
-    </nav>
-    <div>
+    </ul>
+</nav>
+    <div class="projet">
         <h1>PRESENTATION DES PROJETS</h1> <!-- Fermeture correcte de la balise h1 -->
-        
+        <section class="presentation">
         <?php foreach($dataprojet as $projets): ?>
             <h2><?php echo $projets['name']; ?></h2>
             <br>
             <p><?php echo $projets['description']; ?></p>
             <br>
+        </section>
+        <section class="description">
+            <section class="description-groupe">
             <h3>Fonctionnalités</h3>
             <ul>
                 <li><?php echo $projets['fonct_1']; ?></li>
@@ -39,17 +42,24 @@ require_once(__DIR__ . '/variable.php');
                 <li><?php echo $projets['fonct_3']; ?></li>
             </ul>
             <br>
+            </section>
+            <section class="description-groupe">
             <h3>Compétences</h3> <!-- Fermeture correcte de la balise h3 -->
             <ul>
                 <li><?php echo $projets['comp_1']; ?></li>
                 <li><?php echo $projets['comp_2']; ?></li>
             </ul>
             <br>
+            </section>
+            <section class="description-groupe">
             <h3>Logiciels</h3> <!-- Fermeture correcte de la balise h3 -->
             <ul>
                 <li><?php echo $projets['logiciel_1']; ?></li>
                 <li><?php echo $projets['logiciel_2']; ?></li>
             </ul>
+            <br>
+            </section>
+        </section>
         <?php endforeach; ?> <!-- Vous devez fermer la boucle PHP ici -->
     </div>
 </body>
